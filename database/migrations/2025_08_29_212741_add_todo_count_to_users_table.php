04b7ea99c33b787b84440524d5645b55cf19a308
abo3adel <abo3adel35@gmail.com>
2025-08-30 00:32:56 +0300

    feat(users): add trigger for todo count

---
+++ database/migrations/2025_08_29_212741_add_todo_count_to_users_table.php
@@ -0,0 +1,59 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->integer('todo_count')->unsigned()->default(0);
+        });
+
+        DB::unprepared("
+                CREATE TRIGGER after_create_todo
+                AFTER INSERT ON todos
+                FOR EACH ROW
+                BEGIN
+                    UPDATE users
+                    SET todo_count = todo_count + 1
+                    WHERE id = NEW.user_id;
+                END;
+            ");
+
+            DB::unprepared("
+                CREATE TRIGGER after_delete_todo
+                AFTER DELETE ON todos
+                FOR EACH ROW
+                BEGIN
+                    UPDATE users
+                    SET todo_count = GREATEST(todo_count - 1, 0)
+                    WHERE id = OLD.user_id;
+                END;
+            ");
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        DB::unprepared("
+            DROP TRIGGER IF EXISTS after_create_todo;
+            DROP TRIGGER IF EXISTS after_delete_todo;
+        ");
+
+        Schema::table('users', function (Blueprint $table) {
+            $table->dropColumn('todo_count');
+        });
+    }
+};
